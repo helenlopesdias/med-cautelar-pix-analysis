3c8399ad20372d9ef920076f592987c55a82e0a9
Helen Dias <contatohelenlopesdias@gmail.com>
2026-07-02 01:41:31 -0300

    Refactor SQL query to include new calculations

--- AnaliseDetalhada.sql
+++ AnaliseDetalhada.sql
@@ -1,33 +1,89 @@
-SELECT  
-  PARSE_DATE('%Y%m', CAST(AnoMes AS STRING)) AS data_referencia,
-  ValorPixcontestadosaceitos,
-  -- Quanto foi recuperado?
-  (ValorPixdevolvidosintegralmente  + ValorPixdevolvidosparcialmente) AS ValorRecuperado,
+WITH base AS(
 
-  ValorPixnaodevolvidossaldoinsuficiente,
-  Valornaodevolvidoscontaencerrada,
-  ValorPixnaodevolvidosmotivosdiversos,
+SELECT 
+  PARSE_DATE('%Y%m', CAST(AnoMes AS STRING)) AS Data_Referencia,
+  QtdePixcontestados,
+  Qtdecontestacoesaceitas AS QtdFraudes,
+ 
+  QuantidadedevolvidaintegralmentepormeiodoMED AS QtdRecIntegralmente,
+  QuantidadedevolvidaparcialmentepormeiodoMED AS QtdRecParcialmente,
+ 
+  ValorPixcontestadosaceitos AS ValorFraudado,
 
-  -- Qual é a taxa de recuperação do MED?
-  ROUND(
-    (ValorPixdevolvidosintegralmente + ValorPixdevolvidosparcialmente
-    )  / NULLIF(ValorPixcontestadosaceitos, 0), 4) AS TaxaRescuperacaoPercentual,
+  ValorPixdevolvidosintegralmente AS ValorRecIntegralmente,
+  ValorPixdevolvidosparcialmente AS ValorRecParcialmente,
+  
+  ValorPixnaodevolvidossaldoinsuficiente AS ValorNaoRecSaldoInsuf,
+  Valornaodevolvidoscontaencerrada AS ValorNaoRecContaEncer,
+  ValorPixnaodevolvidosmotivosdiversos AS ValorNaoRecMotivosDiv,
+ 
+  ValorPixbloqueadoscautelarmenteedevolvidos AS ValorBloqDevolvido,
+  ValorPixbloqueadoscautelarmenteeliberados AS ValorBloqLiberado,
 
--- Taxa de perda por saldo insuficiente
-    ROUND(
-      ValorPixnaodevolvidossaldoinsuficiente
-      / ValorPixcontestadosaceitos, 4
-    ) AS TaxaPerdaSaldoInsuficiente,
+-- Valor Total Recuperado:
+  (ValorPixdevolvidosintegralmente +
+  ValorPixdevolvidosparcialmente)  AS ValorTotalRec,
 
-    ROUND(
-      ValorPixnaodevolvidosmotivosdiversos
-      / ValorPixcontestadosaceitos, 4
-    ) AS TaxaPerdaMotivoDiv,
+-- Valor Total Não Recuperado 
+    (Valornaodevolvidoscontaencerrada +
+    ValorPixnaodevolvidosmotivosdiversos +
+    ValorPixnaodevolvidossaldoinsuficiente +
+    ValorPixresidualnaodevolvido
+    ) AS ValorTotalNaoRec,
 
-    ROUND(
-      Valornaodevolvidoscontaencerrada
-    )
+-- Valor Total Bloqueado:
+  (ValorPixbloqueadoscautelarmenteeliberados +
+  ValorPixbloqueadoscautelarmenteedevolvidos) AS ValorTotalBloq
+
 FROM 
   `clear-aurora-469314-a0.EstatisticasDoPix.fraude-pix`
-ORDER BY 
-  data_referencia
+
+)
+
+SELECT
+  *,
+-- Taxa de Recuperção por Valor:
+  ROUND(
+    base.ValorTotalRec
+    / NULLIF(base.ValorFraudado, 0)
+    * 100.0, 4) AS TaxaValorRec,
+
+-- Taxa de Não Recuperação por Valor:
+  ROUND(
+    ValorTotalNaoRec 
+    / NULLIF(ValorFraudado, 0)
+    * 100.0,4) AS TaxaValorNaoRec,
+  
+-- Taxa Recuperação Integral Parcialmente)
+  ROUND(
+    (base.QtdRecIntegralmente
+  ) / NULLIF(base.QtdFraudes, 0)
+  * 100.0, 4) AS TaxaQtdRecIntegral,
+
+-- Taxa Recuperação Integral Parcialmente
+  ROUND(
+    (base.QtdRecParcialmente
+    ) / NULLIF(base.QtdFraudes, 0) 
+    * 100.0, 4) AS TaxaQtdRecParcial,
+
+-- Participação do Bloqueio Cautelar:
+  ROUND(
+    base.ValorBloqDevolvido
+    / NULLIF(base.ValorFraudado, 0)
+    * 100.0, 4) AS ParticipacaoBloqCautelar,
+
+-- Taxa dos Bloqueios Devolvidos:
+  ROUND(
+    (base.ValorBloqDevolvido
+  ) / NULLIF(base.ValorTotalBloq, 0)
+  * 100.0,4) AS TaxaValorBloqDevolvido,
+
+-- Taxa dos Bloqueios Liberado:
+  ROUND(
+    (base.ValorBloqLiberado
+    ) /NULLIF(base.ValorTotalBloq, 0)
+  * 100.0,4) AS TaxaValorBloqLiberado
+
+FROM 
+  base
+ORDER BY base.Data_Referencia
